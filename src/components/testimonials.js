2e1a1c7a18c15d47c69ec6f3415883001a354f65
stanley-E-5025 <stanlyumana@Stanlys-MacBook-Air.local>
2022-09-22 10:37:30 -0600

    fix

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -13,7 +13,7 @@ export default function Testimonials() {
           <p>
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico
             en demostraciones de tipografías o de borradores de diseño para
-            probar el diseño visual antes de insertar el texto final.{" "}
+            probar el diseño visual antes de insertar el texto final.
           </p>
           <img alt="img" src={"/self1.jpg"} />
         </div>
@@ -23,7 +23,7 @@ export default function Testimonials() {
           <p>
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico
             en demostraciones de tipografías o de borradores de diseño para
-            probar el diseño visual antes de insertar el texto final.{" "}
+            probar el diseño visual antes de insertar el texto final.
           </p>
           <img alt="img" src={"/self1.jpg"} />
         </div>
@@ -33,7 +33,7 @@ export default function Testimonials() {
           <p>
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico
             en demostraciones de tipografías o de borradores de diseño para
-            probar el diseño visual antes de insertar el texto final.{" "}
+            probar el diseño visual antes de insertar el texto final.
           </p>
           <img alt="img" src={"/self1.jpg"} />
         </div>
